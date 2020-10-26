bc0cf5119573be5c6337e4a90fb320ab9625d6e4
Robert <rbbenett0614@gmail.com>
2020-10-26 22:53:41 +0000

    Grid Array Problem

---
+++ X-Marks-the-Perfect-Shot.js
@@ -0,0 +1,23 @@
+const finalPosition = (moves) => {
+  const coordinates = [0, 0];
+  
+  moves.forEach((move) => {
+    switch(move) {
+      case 'north':
+        coordinates[1] += 1;
+        break;
+      case 'south':
+        coordinates[1] -= 1;
+        break;
+      case 'east':
+        coordinates[0] += 1;
+        break;
+      case 'west':
+        coordinates[0] -= 1;
+        break;
+      default:
+        break;
+    }
+  });
+  return coordinates;
+}
